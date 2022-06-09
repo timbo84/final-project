ee71380b3ce273d7fc833888603a998bdb1ada52
Timothy Roberts <timber110406@gmail.com>
2022-06-09 13:01:57 -0700

    last save

--- script.js
+++ script.js
@@ -1,15 +1,59 @@
-$.ajax(" https://api.adviceslip.com/advice ",{
-    success: function(response){
-        var advice = document.createElement("p");
-            advice.innerHTML = response;
-            console.log(response)
+var myName = document.getElementById("name");
+    myName.addEventListener("mouseenter", function(){
+    myName.style.fontSize = '2.4em';
+})
+myName.addEventListener("mouseleave", function(){
+    myName.style.fontSize = 'calc(1.3rem + .6vw)';
+})
 
-        var header = document.createElement("h3");
-        var title = document.createTextNode("Advice For The Day");
-            header.appendChild(title);
-            document.body.append(header);
-            header.style.textAlign= "center";
-            document.body.append(advice);
-            advice.style.textAlign = "center";
+
+var myJob = document.getElementById("job");
+    myJob.addEventListener("mouseenter", function(){
+    myJob.style.fontSize = '2.4em';
+})
+myJob.addEventListener("mouseleave", function(){
+    myJob.style.fontSize = 'calc(1.3rem + .6vw)';
+})
+$.ajax({
+    url: "https://api.adviceslip.com/advice",
+  
+    dataType: "json",
+    success: function(result){
+        console.log(result)
+
+        var advice = document.getElementById("advice");
+            advice.innerHTML = result.slip.advice;
+        
+         var header = document.getElementById("header");
+             header.style.textAlign= "center";
     }
+})
+        //col1
+var mycol1 = document.getElementById("col1");
+
+mycol1.addEventListener("mouseenter", function(){
+    mycol1.style.backgroundColor ="blue"; 
+})
+mycol1.addEventListener("mouseleave", function(){
+    mycol1.style.backgroundColor ="white";
+})
+
+        //col2
+var myCol2 = document.getElementById("col2");
+
+myCol2.addEventListener("mouseenter", function(){
+    myCol2.style.backgroundColor ="blue"; 
+})
+myCol2.addEventListener("mouseleave", function(){
+    myCol2.style.backgroundColor ="white";
+})
+
+        //col3
+var myCol3 = document.getElementById("col3");
+
+myCol3.addEventListener("mouseenter", function(){
+    myCol3.style.backgroundColor ="blue"; 
+})
+myCol3.addEventListener("mouseleave", function(){
+    myCol3.style.backgroundColor ="white";
 })
